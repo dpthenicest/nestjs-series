af2b92ca4db23f5d7194b2d7b7cf5dab7bbcbe54
dpthenicest <fortuneprecious17@gmail.com>
2025-11-06 11:47:13 +0100

    feat: initialize NestJS application with basic structure and user module
    - Added package.json with necessary dependencies and scripts
    - Created AppController and AppService with a simple "Hello World!" endpoint
    - Implemented UsersModule with UsersController and UsersService
    - Added unit tests for AppController, UsersController, AppService, and UsersService
    - Set up Jest for testing with coverage and e2e testing configuration
    - Configured TypeScript with appropriate settings for the project

---
+++ src/users/users.controller.ts
@@ -0,0 +1,27 @@
+import { Controller, Get, Param } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  /*
+  GET /users
+  GET /users/:id
+  POST /users
+  PATCH /users:id
+  DELETE /users/:id
+  */
+
+  @Get() // GET /users
+  findAll() {
+    return [];
+  }
+
+  @Get() //GET /users/:id
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Get('interns') // GET /users/interns
+  findAllInterns() {
+    return [];
+  }
+}
